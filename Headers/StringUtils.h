2402370b9409d6ac1a86e5848d51907f4286824d
huangxieyue <huangxieyue@hotmail.com>
2020-08-25 14:14:59 +0800

    Add files via upload

---
+++ Headers/StringUtils.h
@@ -0,0 +1,19 @@
+//
+//  StringUtils.h
+//  demoFramework
+//
+//  Created by Debo on 2020/8/24.
+//  Copyright © 2020 Debo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface StringUtils : NSObject
+
++ (NSString *)testString:(NSString *)string;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Headers/demoFramework.h
@@ -0,0 +1,20 @@
+//
+//  demoFramework.h
+//  demoFramework
+//
+//  Created by Debo on 2020/8/24.
+//  Copyright © 2020 Debo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "StringUtils.h"
+
+//! Project version number for demoFramework.
+FOUNDATION_EXPORT double demoFrameworkVersionNumber;
+
+//! Project version string for demoFramework.
+FOUNDATION_EXPORT const unsigned char demoFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <demoFramework/PublicHeader.h>
+
+
